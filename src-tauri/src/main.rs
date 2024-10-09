8f728967f1ff6ca5bb41f7adba50ea898d3a5eae
Iewnfod <shangzhiqiuyue@outlook.com>
2024-10-09 12:14:55 +0800

    feat: tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,7 +7,10 @@ mod structs;
 mod etag;
 
 use net::*;
-use tauri::{AppHandle, Manager, RunEvent, WindowEvent};
+use tauri::{
+    App, AppHandle, Builder, CustomMenuItem, Manager, RunEvent, Runtime, SystemTray,
+    SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem, WindowEvent,
+};
 
 fn main_loop(app_handle: &AppHandle, event: RunEvent) {
     match event {
@@ -34,6 +37,38 @@ fn main_loop(app_handle: &AppHandle, event: RunEvent) {
     }
 }
 
+fn build_tray() -> SystemTray {
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(CustomMenuItem::new("show".to_string(), "Show Window").accelerator("Command+S"))
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Command+Q"));
+
+    SystemTray::new().with_menu(tray_menu)
+}
+
+fn tray_event(app_handle: &AppHandle, event: SystemTrayEvent) {
+    match event {
+        SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+            "quit" => {
+                std::process::exit(0);
+            }
+            "show" => {
+                let window = app_handle.get_window("main").unwrap();
+
+                #[cfg(target_os = "macos")]
+                tauri::AppHandle::show(&window.app_handle()).unwrap();
+
+                #[cfg(target_os = "linux")]
+                window.show().unwrap();
+
+                window.set_focus().unwrap();
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+}
+
 fn main() {
     let app = tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
@@ -48,6 +83,8 @@ fn main() {
             set_username,
             set_password
         ])
+        .system_tray(build_tray())
+        .on_system_tray_event(tray_event)
         .build(tauri::generate_context!()).unwrap();
 
     app.run(main_loop);
